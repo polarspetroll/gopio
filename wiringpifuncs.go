fbfe969219cbc5fb33d49fc4c43520c2bc8c2060
polarspetroll <polarspetroll@gmail.com>
2021-09-30 23:07:32 +0330

    Add comments

--- wiringpifuncs.go
+++ wiringpifuncs.go
@@ -6,10 +6,13 @@ package gopio
 */
 import "C"
 
+// initial function for wiringpi(call this function if you're going to use wiringpi)
 func GopioSetUp() {
   C.wiringPiSetupPhys()
 }
 
+// define a wiringpi pin using this function
+// pnum : physical pin number, d direction
 func PinMode(pnum int, d DIR) WiringPiPin {
   if d == OUT {
     C.pinMode(C.int(pnum), C.OUTPUT)
@@ -20,6 +23,7 @@ func PinMode(pnum int, d DIR) WiringPiPin {
 }
 
 
+// digital write accept two values HIGH and LOW
 func (p *WiringPiPin) DigitalWrite(v VALUE) {
   if v == HIGH {
     C.digitalWrite(C.int(p.Num), C.HIGH)
@@ -28,6 +32,7 @@ func (p *WiringPiPin) DigitalWrite(v VALUE) {
   }
 }
 
+// digitalread function returns the current value for a specific pin
 func (p *WiringPiPin) DigitalRead() VALUE {
   v := int(C.digitalRead(C.int(p.Num)))
   if v == 1 {
