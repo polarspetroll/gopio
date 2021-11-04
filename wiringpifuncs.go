dadb3f50f94d5de39d971307ce63ce0f051dde92
polarspetroll <polarspetroll@gmail.com>
2021-11-04 09:29:17 +0330

    Bug fix

--- wiringpifuncs.go
+++ wiringpifuncs.go
@@ -9,57 +9,56 @@ import "C"
 
 // initial function for wiringpi(call this function if you're going to use wiringpi)
 func GopioSetUp() {
-  C.wiringPiSetupPhys()
+	C.wiringPiSetupPhys()
 }
 
 // define a wiringpi pin using this function
 // pnum : physical pin number, d direction
 func PinMode(pnum int, d DIR) WiringPiPin {
-  if d == OUT {
-    C.pinMode(C.int(pnum), C.OUTPUT)
-  }else {
-    C.pinMode(C.int(pnum), C.INPUT)
-  }
-  return WiringPiPin{Num: pnum, Dir: d}
+	if d == OUT {
+		C.pinMode(C.int(pnum), C.OUTPUT)
+	} else {
+		C.pinMode(C.int(pnum), C.INPUT)
+	}
+	return WiringPiPin{Num: pnum, Dir: d}
 }
-
 
 // digital write accept two values HIGH and LOW
 func (p *WiringPiPin) DigitalWrite(v VALUE) {
-  if v == HIGH {
-    C.digitalWrite(C.int(p.Num), C.HIGH)
-  }else {
-    C.digitalWrite(C.int(p.Num), C.LOW)
-  }
+	if v == HIGH {
+		C.digitalWrite(C.int(p.Num), C.HIGH)
+	} else {
+		C.digitalWrite(C.int(p.Num), C.LOW)
+	}
 }
 
 // digitalread function returns the current value for a specific pin
 func (p *WiringPiPin) DigitalRead() VALUE {
-  v := int(C.digitalRead(C.int(p.Num)))
-  if v == 1 {
-    return HIGH
-  }else {
-    return LOW
-  }
+	v := int(C.digitalRead(C.int(p.Num)))
+	if v == 1 {
+		return HIGH
+	} else {
+		return LOW
+	}
 }
 
 // this function takes a value(HIGH, LOW) and writes it to the pin on program interruption
-func (p WiringPiPin) WriteOnInterrupt(v VALUE) {
-  var val int
-  if v == HIGH {
-    val = 1
-  }else {
-    val = 0
-  }
-  C.close_pin(C.int(p.Num), C.int(val))
+func (p *WiringPiPin) WriteOnInterrupt(v VALUE) {
+	var val int
+	if v == HIGH {
+		val = 1
+	} else {
+		val = 0
+	}
+	C.close_pin(C.int(p.Num), C.int(val))
 }
 
 // returns the analog value for pin(not tested)
-func (p WiringPiPin) AnalogRead() int {
-  return int(C.analogRead(C.int(p.Num)))
+func (p *WiringPiPin) AnalogRead() int {
+	return int(C.analogRead(C.int(p.Num)))
 }
 
 // writes analog value to pin(not tested)
-func (p WiringPiPin) AnalogWrite(val int) {
-  C.analogWrite(C.int(val))
+func (p *WiringPiPin) AnalogWrite(val int) {
+	C.analogWrite(C.int(p.Num), C.int(val))
 }
